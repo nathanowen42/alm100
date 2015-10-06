855da745c625411a2319482ca3d6742ce2f8337c
Nathan Owen <nathanowen42@gmail.com>
2015-10-06 18:46:13 -0500

    small fixes to uboot dir

--- opensource/uboot/release.sh
+++ opensource/uboot/release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-RELEASE_TAR=`pwd`/../../releases/boot
-BUILD_DIR=`pwd`/ti-u-boot/am335x_emv
+RELEASE_TAR=`pwd`/../../releases/boot/
+BUILD_DIR=`pwd`/ti-u-boot/am335x_evm/
 cp -v ${BUILD_DIR}/u-boot ${RELEASE_TAR}
 cp -v ${BUILD_DIR}/spl/u-boot-spl.bin ${RELEASE_TAR} 
 cp -v ${BUILD_DIR}/spl/u-boot-spl ${RELEASE_TAR} 
