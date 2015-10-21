8ccb7124e4b10abe7fb0dec35aded53e9df6a2c2
Nathan Owen <nathanowen42@gmail.com>
2015-10-21 16:41:14 -0500

    qt changes in progress

--- opensource/qt/build_qt.sh
+++ opensource/qt/build_qt.sh
@@ -2,10 +2,14 @@
 TOPDIR=`pwd`
 QT_DIR=${TOPDIR}/qt
 CONFIG_FILE=${TOPDIR}/configs/qmake.conf
+INSTALL_PATH=${TOPDIR}/build
 
 cp -v ${CONFIG_FILE} ${QT_DIR}/mkspecs/qws/linux-arm-g++/qmake.conf
 
 cd ${QT_DIR}
-./configure -prefix <PATH> -embedded arm -platform qws/linux-x86-g++ -xplatform qws/linux-omap3-g++ -depths 16,24,32 -no-mmx 
--no-3dnow -no-sse -no-sse2 -no-glib -no-cups -no-largefile -no-accessibility -no-openssl -no-gtkstyle -qt-mouse-pc -qt-mouse-linuxtp 
--qt-mouse-linuxinput -plugin-mouse-linuxtp -plugin-mouse-pc -fast 
+
+./configure -opensource -little-endian -prefix ${INSTALL_PATH} -embedded arm -platform qws/linux-x86-g++ -xplatform qws/linux-arm-g++ -depths 16,24,32 -no-mmx -no-3dnow -no-sse -no-sse2 -no-glib -no-cups -no-largefile -no-accessibility -no-openssl -no-gtkstyle -qt-mouse-pc -qt-mouse-linuxtp -qt-mouse-linuxinput -plugin-mouse-linuxtp -plugin-mouse-pc -fast 
+
+make clean
+make
+#make install
